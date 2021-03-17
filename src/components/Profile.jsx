74088d0dea457ee0b23d59bcec1f941a963995e5
DeveloperMAI <azatu421@gmail.com>
2021-03-17 19:09:58 +0300

    added module-css

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,13 @@
 import React, { component } from "react";
-
+import s from "./profile.module.css"
 const Profile=()=>{
     return(
-        <div className="content">
+        <div className={s.content}>
             <div className="myprofile">My profile</div>
-        <div className="Image" > <img src="https://static.locals.md/2014/06/Yuri-Gagarin-1152.jpg" width="400px" /> </div>
+        <div className={s.Image} > <img src="https://static.locals.md/2014/06/Yuri-Gagarin-1152.jpg" width="400px" /> </div>
        
         <div>
-            <div className="newpost">New post </div>
+            <div className={s.newpost}>New post </div>
           <div> post1</div>
           <div>post2</div>
         </div>
